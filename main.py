13b1d92156175850578945e82562ef9151a9a5fc
twag117 <53270609+twag117@users.noreply.github.com>
2025-04-28 17:29:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/recipes")
 async def read_recipes():
     return [{"id": 1, "title": "Spaghetti"}, {"id": 2, "title": "Tacos"}]
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
